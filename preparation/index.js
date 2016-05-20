e104b4834ed7dd32941b9d555b85dbab84fcf7ba
Anson <s1496k030@gmail.com>
2016-05-20 14:45:34 +0800

    Move converter, sources, and data to EvacuationRESTful.

--- preparation/index.js
+++
@@ -1,8 +0,0 @@
-var Converter = require('./converter'),
-    converter = new Converter();
-
-converter.villageDataCSV2JSON();
-// converter.villagePopulationCSV2JSON();
-// converter.villageHeadJSONArrange();
-
-// converter.villageShelterXML2JSON();
